635e6ebce52d786f94ae75f36fd9b2d3530dca95
prasanth <prasanthkumarv1999@gmail.com>
2026-06-15 11:20:14 +0530

    first commit

--- Day1/Numpy/Numpy.py
+++ Day1/Numpy/Numpy.py
@@ -1,16 +1,27 @@
 import numpy as np
 
-arr = numpy.array([1,2,3,4,5])
+# 1-D Arrays
+arr = np.array([1,2,3,4,5])
 
 print(arr)
-print(numpy.__version__)
+print(np.__version__)
 
-arr = np.array([1, 2, 3, 4, 5])
-
-print(arr)
-
-import numpy as np
+# 2-D Arrays
 
 arr = np.array([[1, 2, 3], [4, 5, 6]])
 
 print(arr)
+
+3# -D arrays
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
