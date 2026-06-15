3caac06614fbe203f4855d8ac9444f91b6ddd52f
prasanth <prasanthkumarv1999@gmail.com>
2026-06-15 11:17:25 +0530

    Add NumPy learning files

---
+++ Day1/Numpy/Numpy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr = numpy.array([1,2,3,4,5])
+
+print(arr)
+print(numpy.__version__)
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr)
