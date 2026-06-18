107442ba6b1bb56d81eeaeed5c2e5687dd6ddd50
prasanth <prasanthkumarv1999@gmail.com>
2026-06-18 13:02:22 +0530

    Scikit Changes

---
+++ Day3/Scikit_Learn_Test.py
@@ -0,0 +1,123 @@
+# If we train 100% data it will predict all the questions correctly 
+# instead we use 80% data is to train and 20% data is to test.
+
+# 100 records == students
+# 80 records == train
+# 20 records == test
+
+# 1) train_test_split :
+
+# from sklearn.model_selection import train_test_split
+
+# x= [[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]]
+# y= [[10],[20],[30],[40],[50],[60],[70],[80],[90],[100]]
+
+# # random_state is used to block the random array data generation if you resume every file run it will generate a random array data
+
+# x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2, random_state=52)
+
+# print("X_train", x_train)
+# print("Y_train", y_train)
+# print("X_test", x_test)
+# print("Y_test", y_test)
+
+# 2) Linear Regression : Oka input (X) ki output (y) madhya relationship ni nerchukuni future values predict cheyyadam.
+#          ⬇️
+    # Straight Line Graph ==> ONLY Numerical Data 
+
+# If you want to use other data it should be encoded to numerical data
+
+# Series = Single column = []  | Series  → Numerical + Text + Dates + Categories
+
+# Wrong Code : here two columns 
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+data = {
+    "Experience" : [1,2,3,4,5],
+    "Salary" : [20000,30000,40000,50000,60000]
+}
+
+df = pd.Series(data)
+
+X = df["Experience"]
+y = df["Salary"]
+
+model = LinearRegression()
+
+model.fit(X,y)
+
+prediction = model.predict([6])
+
+print(prediction)
+
+# Correct Code : 
+
+import pandas as pd
+
+experience = pd.Series([1,2,3,4,5])
+
+print(experience)
+
+
+# DataFrame = Rows & Columns = [[]] | DataFrame → Numerical + Text + Dates + Categories
+
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name": ["John","David","Mike"],
+    "Age": [22,25,28],
+    "Salary": [30000,40000,50000],
+    "Is_Employee": [True,True,False]
+})
+
+print(df)
+
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+data = {
+    "Experience" : [1,2,3,4,5],
+    "Salary" : [20000,30000,40000,50000,60000]
+}
+
+df = pd.DataFrame(data)
+
+X = df[["Experience"]]
+y = df["Salary"]
+
+model = LinearRegression()
+
+model.fit(X,y)
+
+prediction = model.predict([[6]])
+
+print(prediction)
+
+# 3) Model Evalution : Comparing Actual Data(Marks) to Predicted Data(Marks)
+
+    # How to check model predicting correct answers : By Using
+
+        # 1) MAE (Mean Absolute Error) : Most beginner-friendly metric. [MAE = Average of Errors]
+
+                # Example:   Actual	    Predicted        Errors         Average 
+                #             100	        90             10       [sum of errors/no.of errors]
+                #             80	        70             10       (10+10+10)/3 = 10
+                #             60	        50             10
+                #           "Here MAE == 10"
+
+from sklearn.metrics import mean_absolute_error
+import pandas as pd
+
+actual = [100,80,60]
+
+predicted = [90,70,50]
+
+MAE = mean_absolute_error(
+    actual, predicted
+)
+
+print(MAE)
+
+
+
